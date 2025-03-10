620a969a5c9af9c3c4cd2772507617698e6f9a22
O-Dong <it0825@naver.com>
2025-03-10 18:44:53 +0900

    실습3: @@님 안녕하세요!

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -2,13 +2,17 @@ package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
-@Controller
+@Controller // 컨트롤러 선언
 public class FirstController {
 
-  @GetMapping("/hi")
-  public String niceToMeetYou() {
-    return "greetings";
+  @GetMapping("/hi") // 클라이언트로부터 "/hi"라는 요청을 받아 접수
+
+  // "hi"라는 요청을 받음과 동시에 niceToMeetYou() 메서드를 수행
+  public String niceToMeetYou(Model model) { // 모델 객체를 매개변수로 가져옴
+    model.addAttribute("username", "seongeun"); // 모델에서 사용할 변수를 등록
+    return "greetings"; // 메서드를 수행한 결과로 greetings.mustache 파일을 반환
   }
 
 }
