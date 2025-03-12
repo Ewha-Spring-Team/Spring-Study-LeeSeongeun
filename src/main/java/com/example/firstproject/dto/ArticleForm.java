610bd96ba961befd782eb0ddd0aa10ea36d278bc
O-Dong <it0825@naver.com>
2025-03-12 23:44:11 +0900

    실습13: DTO 리팩터링하기

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,28 +1,32 @@
 package com.example.firstproject.dto;
 
+import lombok.AllArgsConstructor; // AllArgsConstructor 패키지 임포트
+import lombok.ToString; // ToString 패키지 임포트
 import com.example.firstproject.entity.Article;
 
+@AllArgsConstructor // 새 어노테이션 추가
+@ToString // 새 어노테이션 추가
 public class ArticleForm {
-  private String title; // 제목을 받을 필드
-  private String content; // 내용을 받을 필드
+  private String title;
+  private String content;
 
   // 전송받은 제목과 내용을 필드에 저장하는 생성자 추가
-  public ArticleForm(String title, String content) {
-    this.title = title;
-    this.content = content;
-  }
+  // public ArticleForm(String title, String content) {
+  // this.title = title;
+  // this.content = content;
+  // }
 
   // 데이터를 잘 받았는지 확인할 toString() 메서드 추가
-  @Override
-  public String toString() {
-    return "ArticleForm{" +
-        "title='" + title + '\'' +
-        ", content='" + content + '\'' +
-        '}';
-  }
+  // @Override
+  // public String toString() {
+  // return "ArticleForm{" +
+  // "title='" + title + '\'' +
+  // ", content='" + content + '\'' +
+  // '}';
+  // }
 
   // 폼 데이터를 담은 DTO 객체를 엔티티로 반환
   public Article toEntity() {
-    return new Article(null, title, content); // 생성자 입력 양식에 맞게 작성
+    return new Article(null, title, content);
   }
 }
