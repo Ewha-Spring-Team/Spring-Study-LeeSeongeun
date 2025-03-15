522c2b07ddd6df930f0fa8d2cda6937330d128d4
O-Dong <it0825@naver.com>
2025-03-16 00:58:31 +0900

    실습3: 5장 셀프체크

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.firstproject.controller;
 
+import java.util.ArrayList;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,31 +9,50 @@ import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.repository.MemberRepository;
 import com.example.firstproject.entity.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 
-@Slf4j // 로깅 기능을 위한 어노테이션 추가
-@Controller // 컨트롤러 선언
+@Slf4j
+@Controller
 public class MemberController {
 
-  @Autowired // 스프링 부트가 미리 생성한 둔 리파지터리 객체 주입
+  @Autowired
   private MemberRepository memberRepository;
 
-  @GetMapping("/members/new") // url 요청 접수
-  public String newMemberForm() { // 메서드 생성 및 반환값 작성
-    return "members/new"; // members/new 페이지를 반환
+  @GetMapping("/members/new")
+  public String newMemberForm() {
+    return "members/new";
   }
 
-  // 폼 데이터를 받아오는 작업
-  @PostMapping("/join") // url 요청 접수
+  @PostMapping("/join")
   public String joinMember(MemberForm form) {
     log.info(form.toString());
 
-    // DTO를 엔티티로 변환
     Member member = form.toEntity();
     log.info(member.toString());
 
-    // 리파지터리로 엔티티를 DB에 저장
     Member saved = memberRepository.save(member);
     log.info(saved.toString());
-    return "";
+    return "articles/show";
+  }
+
+  @GetMapping("/members/{id}")
+  public String show(@PathVariable Long id, Model model) { // ㄱ, ㄴ 정답
+    // 1. id를 조회해 데이터 가져오기
+    Member memberEntity = memberRepository.findById(id).orElse(null); // ㄷ 정답
+    // 2. 모델에 데이터 등록하기
+    model.addAttribute("member", memberEntity); // ㄹ 정답
+    // 3. 뷰 페이지 반환하기
+    return "members/show";
+  }
+
+  @GetMapping("/members")
+  public String index(Model model) { // ㅁ 정답
+    // 1. 모든 데이터 가져오기
+    ArrayList<Member> memberEntityList = memberRepository.findAll(); // ㅂ 정답
+    // 2. 모델에 데이터 등록하기
+    model.addAttribute("memberList", memberEntityList); // ㅅ 정답
+    // 3. 뷰 페이지 설정하기기
+    return "members/index";
   }
 }
--- src/main/java/com/example/firstproject/entity/Member.java
+++ src/main/java/com/example/firstproject/entity/Member.java
@@ -2,13 +2,19 @@ package com.example.firstproject.entity;
 
 import java.lang.annotation.Inherited;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 import javax.annotation.processing.Generated;
+import lombok.NoArgsConstructor; // @NoArgsConstructor 임포트
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+@AllArgsConstructor
+@NoArgsConstructor // 기본 생성자를 추가 해주는 어노테이션
+@ToString
 @Entity // 이 클래스가 엔티티임을 선언
 public class Member {
   @Id
@@ -18,21 +24,4 @@ public class Member {
   private String email;
   @Column
   private String password;
-
-  // Member 생성자 추가
-  public Member(Long id, String email, String password) {
-    this.id = id;
-    this.email = email;
-    this.password = password;
-  }
-
-  // 오버라이드
-  @Override
-  public String toString() {
-    return "Member{" +
-        "id=" + id +
-        ", email='" + email + '\'' +
-        ", password='" + password + '\'' +
-        '}';
-  }
 }
