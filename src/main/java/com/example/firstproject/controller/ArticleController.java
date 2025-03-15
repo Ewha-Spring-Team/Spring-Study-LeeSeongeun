675bf819becf0f3c2af8910a5dee8906dcfa2609
O-Dong <it0825@naver.com>
2025-03-15 13:48:07 +0900

    실습2: 데이터 목록 조회하기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,5 +1,6 @@
 package com.example.firstproject.controller;
 
+import java.util.ArrayList;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +24,7 @@ public class ArticleController {
     return "articles/new";
   }
 
-  @GetMapping("/articles/{id}") // id를 변수로!
+  @GetMapping("/articles/{id}")
   public String show(@PathVariable Long id, Model model) {
     log.info("id = " + id);
     // 1. id를 조회해 데이터 가져오기
@@ -32,6 +33,16 @@ public class ArticleController {
     model.addAttribute("article", articleEntity);
     // 3. 뷰 페이지 반환하기
     return "articles/show";
+  }
+
+  @GetMapping("/articles")
+  public String index(Model model) { // model 객체 받아 오기
+    // 1. 모든 데이터 가져오기
+    ArrayList<Article> articleEntityList = articleRepository.findAll();
+    // 2. 모델에 데이터 등록하기
+    model.addAttribute("articleList", articleEntityList); // articleEntity 등록
+    // 3. 뷰 페이지 설정하기
+    return "articles/index";
   }
 
   @PostMapping("/articles/create")
