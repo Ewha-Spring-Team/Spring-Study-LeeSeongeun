feaca976900b11c28f226c3c5379a94032713230
O-Dong <it0825@naver.com>
2025-03-15 12:24:21 +0900

    실습1: 단일 데이터 조회하기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -8,33 +8,41 @@ import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.repository.ArticleRepository;
 import com.example.firstproject.entity.Article;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 
-@Slf4j // 로깅 기능을 위한 어노테이션 추가
+@Slf4j
 @Controller
 public class ArticleController {
 
   @Autowired
-  private ArticleRepository articleRepository; // articleRepository 객체 선언
+  private ArticleRepository articleRepository;
 
   @GetMapping("/articles/new")
   public String newArticleForm() {
     return "articles/new";
   }
 
+  @GetMapping("/articles/{id}") // id를 변수로!
+  public String show(@PathVariable Long id, Model model) {
+    log.info("id = " + id);
+    // 1. id를 조회해 데이터 가져오기
+    Article articleEntity = articleRepository.findById(id).orElse(null);
+    // 2. 모델에 데이터 등록하기
+    model.addAttribute("article", articleEntity);
+    // 3. 뷰 페이지 반환하기
+    return "articles/show";
+  }
+
   @PostMapping("/articles/create")
   public String createArticle(ArticleForm form) {
     log.info(form.toString());
-    // System.out.println(form.toString()); 기존 println
 
-    // 1. DTO를 엔티티로 변환
     Article article = form.toEntity();
     log.info(article.toString());
-    // System.out.println(article.toString()); 기존 println
 
-    // 2. 리파지터리로 엔티티를 DB에 저장
-    Article saved = articleRepository.save(article); // 엔티티를 저장해 saved 객체에 반환
+    Article saved = articleRepository.save(article);
     log.info(saved.toString());
-    // System.out.println(saved.toString()); 기존 println
 
     return "";
   }
