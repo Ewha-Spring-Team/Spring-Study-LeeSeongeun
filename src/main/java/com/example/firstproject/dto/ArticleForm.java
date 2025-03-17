e0428e118800b49b3ea5f3120f1a492eb6ec0202
O-Dong <it0825@naver.com>
2025-03-18 01:07:01 +0900

    실습7: 수정이 반영된 페이지 리다이렉트

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,32 +1,17 @@
 package com.example.firstproject.dto;
 
-import lombok.AllArgsConstructor; // AllArgsConstructor 패키지 임포트
-import lombok.ToString; // ToString 패키지 임포트
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 import com.example.firstproject.entity.Article;
 
-@AllArgsConstructor // 새 어노테이션 추가
-@ToString // 새 어노테이션 추가
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
+  private Long id; // id 필드 추가
   private String title;
   private String content;
 
-  // 전송받은 제목과 내용을 필드에 저장하는 생성자 추가
-  // public ArticleForm(String title, String content) {
-  // this.title = title;
-  // this.content = content;
-  // }
-
-  // 데이터를 잘 받았는지 확인할 toString() 메서드 추가
-  // @Override
-  // public String toString() {
-  // return "ArticleForm{" +
-  // "title='" + title + '\'' +
-  // ", content='" + content + '\'' +
-  // '}';
-  // }
-
-  // 폼 데이터를 담은 DTO 객체를 엔티티로 반환
   public Article toEntity() {
-    return new Article(null, title, content);
+    return new Article(id, title, content); // null -> id로 수정
   }
 }
