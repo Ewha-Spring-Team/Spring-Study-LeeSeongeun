a481d5bf7d265e8d14911f6202aa78d0a094631d
O-Dong <it0825@naver.com>
2025-03-25 15:55:01 +0900

    실습3: REST API - PATCH 구현

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -1,13 +1,23 @@
 package com.example.firstproject.api;
 
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
+@Slf4j
 @RestController
+
 public class ArticleApiController {
   @Autowired // 게시글 리파지터리 주입
   private ArticleRepository articleRepository;
@@ -16,5 +26,35 @@ public class ArticleApiController {
   @GetMapping("/api/articles")
   public List<Article> index() {
     return articleRepository.findAll();
+  }
+
+  @GetMapping("/api/articles/{id}")
+  public Article show(@PathVariable Long id) {
+    return articleRepository.findById(id).orElse(null);
+  }
+
+  // POST
+  @PostMapping("/api/articles")
+  public Article create(@RequestBody ArticleForm dto) {
+    Article article = dto.toEntity();
+    return articleRepository.save(article);
+  }
+
+  // PATCH
+  @PatchMapping("/api/articles/{id}")
+  public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
+    // 1. DTO -> 엔티티 변환하기
+    Article article = dto.toEntity();
+    log.info("id: {}, article: {}", id, article.toString());
+    // 2. 타깃 조회하기
+    Article target = articleRepository.findById(id).orElse(null);
+    // 3. 잘못된 요청 처리하기
+    if (target == null || id != article.getId()) {
+      log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+    }
+    // 4. 업데이트 및 정상 응답(200)하기
+    Article updated = articleRepository.save(article);
+    return ResponseEntity.status(HttpStatus.OK).body(updated);
   }
 }
