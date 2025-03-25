5e9977a84a63713ed94f74d5b1fc27a39fbd8ac9
O-Dong <it0825@naver.com>
2025-03-25 16:05:55 +0900

    실습4: REST API - DELETE 구현

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
@@ -54,7 +55,22 @@ public class ArticleApiController {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
     }
     // 4. 업데이트 및 정상 응답(200)하기
-    Article updated = articleRepository.save(article);
+    target.patch(article);
+    Article updated = articleRepository.save(target);
     return ResponseEntity.status(HttpStatus.OK).body(updated);
+  }
+
+  // DELETE
+  @DeleteMapping("/api/articles/{id}")
+  public ResponseEntity<Article> delete(@PathVariable Long id) {
+    // 1. 대상 찾기
+    Article target = articleRepository.findById(id).orElse(null);
+    // 2. 잘못된 요청 처리하기
+    if (target == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+    }
+    // 3. 대상 삭제하기
+    articleRepository.delete(target);
+    return ResponseEntity.status(HttpStatus.OK).body(null);
   }
 }
