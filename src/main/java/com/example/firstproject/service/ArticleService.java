9e2bc883b601367dc40edecf854826eaaf4c8cc6
O-Dong <it0825@naver.com>
2025-03-26 15:59:25 +0900

    실습7: 서비스로 모두 분리하기

--- src/main/java/com/example/firstproject/service/ArticleService.java
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -1,12 +1,14 @@
 package com.example.firstproject.service;
 
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.firstproject.dto.ArticleForm;
 
+@Slf4j
 @Service // 서비스 객체 생성
 public class ArticleService {
   @Autowired
@@ -26,5 +28,34 @@ public class ArticleService {
       return null;
     }
     return articleRepository.save(article); // article을 DB에 저장
+  }
+
+  public Article update(Long id, ArticleForm dto) {
+    // 1. DTO -> 엔티티 변환하기
+    Article article = dto.toEntity();
+    log.info("id: {}, article: {}", id, article.toString());
+    // 2. 타깃 조회하기
+    Article target = articleRepository.findById(id).orElse(null);
+    // 3. 잘못된 요청 처리하기
+    if (target == null || id != article.getId()) {
+      log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+      return null; // 응답은 컨트롤러가 하므로 여기서는 null 반환
+    }
+    // 4. 업데이트 및 정상 응답(200)하기
+    target.patch(article);
+    Article updated = articleRepository.save(target);
+    return updated; // 응답은 컨트롤러가 하므로 여기서는 수정 데이터만 반환
+  }
+
+  public Article delete(Long id) {
+    // 1. 대상 찾기
+    Article target = articleRepository.findById(id).orElse(null);
+    // 2. 잘못된 요청 처리하기
+    if (target == null) {
+      return null;
+    }
+    // 3. 대상 삭제하기
+    articleRepository.delete(target);
+    return target;
   }
 }
