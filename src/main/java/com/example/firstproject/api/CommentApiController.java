eeb5b2a1d2afbd336dfbbe3d897ce6fc743d1a0d
O-Dong <it0825@naver.com>
2025-04-02 11:05:37 +0900

    실습11: 댓글 삭제하기

--- src/main/java/com/example/firstproject/api/CommentApiController.java
+++ src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,5 +47,13 @@ public class CommentApiController {
     // 결과 응답
     return ResponseEntity.status(HttpStatus.OK).body(updatedDto);
   }
+
   // 4. 댓글 삭제
+  @DeleteMapping("/api/comments/{id}")
+  public ResponseEntity<CommentDto> delete(@PathVariable Long id) {
+    // 서비스에 위임
+    CommentDto deletedDto = commentService.delete(id);
+    // 결과 응답
+    return ResponseEntity.status(HttpStatus.OK).body(deletedDto);
+  }
 }
